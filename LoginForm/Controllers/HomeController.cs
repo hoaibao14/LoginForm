89647600498cd411f7c7f189d540c2eda5373b1e
hoaibao14 <lahoaibao14@gmail.com>
2017-10-28 01:51:40 +0700

    visual

---
+++ LoginForm/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Table usr)
+        { 
+            if (ModelState.IsValid)
+            {
+                using (UserInfoEntities uie = new UserInfoEntities())
+                {
+                    var v = uie.Tables.Where(a => a.Username.Equals(usr.Username) && a.Password.Equals(usr.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.UserID.ToString();
+                        Session["LogedUserFullname"] = v.Fullname.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(usr);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("index");
+            }
+            
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[UserID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Fullname] NCHAR(50) NOT NULL, 
+    [Username] NCHAR(30) NOT NULL, 
+    [Password] NCHAR(30) NOT NULL
+)
